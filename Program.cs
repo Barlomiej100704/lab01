9f65580f161ab392609ab3a4d0e864aad46b744a
Barlomiej100704 <129675337+Barlomiej100704@users.noreply.github.com>
2023-04-02 17:15:11 +0200

    Test aktulizacji

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Rozpoczynamy zajęcia");
+Console.WriteLine("Rozpoczynamy zajęcia!");
+Console.WriteLine("Ćwiczenia trwają!");
+
